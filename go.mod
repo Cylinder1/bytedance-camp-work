2075b14d422eb40c2ab6a65cf1ecc3ef1515047c
Cylinder1 <2570522968@qq.com>
2022-05-10 17:04:26 +0800

    work1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module main/bytedance-camp-work
+
+go 1.18
---
+++ work1/2/main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+type DictRequest struct {
+	Text     string `json:"text"`
+	Language string `json:"language"`
+}
+
+type DictRequest1 struct {
+	TransType string `json:"trans_type"`
+	Source    string `json:"source"`
+	UserID    string `json:"user_id"`
+}
+
+type DictResponse struct {
+	Words []struct {
+		Source  int    `json:"source"`
+		Text    string `json:"text"`
+		PosList []struct {
+			Type      int `json:"type"`
+			Phonetics []struct {
+				Type int    `json:"type"`
+				Text string `json:"text"`
+			} `json:"phonetics"`
+			Explanations []struct {
+				Text     string `json:"text"`
+				Examples []struct {
+					Type      int `json:"type"`
+					Sentences []struct {
+						Text      string `json:"text"`
+						TransText string `json:"trans_text"`
+					} `json:"sentences"`
+				} `json:"examples"`
+				Synonyms []interface{} `json:"synonyms"`
+			} `json:"explanations"`
+			Relevancys []interface{} `json:"relevancys"`
+		} `json:"pos_list"`
+	} `json:"words"`
+	Phrases  []interface{} `json:"phrases"`
+	BaseResp struct {
+		StatusCode    int    `json:"status_code"`
+		StatusMessage string `json:"status_message"`
+	} `json:"base_resp"`
+}
+
+type DictResponse1 struct {
+	Rc   int `json:"rc"`
+	Wiki struct {
+		KnownInLaguages int `json:"known_in_laguages"`
+		Description     struct {
+			Source string      `json:"source"`
+			Target interface{} `json:"target"`
+		} `json:"description"`
+		ID   string `json:"id"`
+		Item struct {
+			Source string `json:"source"`
+			Target string `json:"target"`
+		} `json:"item"`
+		ImageURL  string `json:"image_url"`
+		IsSubject string `json:"is_subject"`
+		Sitelink  string `json:"sitelink"`
+	} `json:"wiki"`
+	Dictionary struct {
+		Prons struct {
+			EnUs string `json:"en-us"`
+			En   string `json:"en"`
+		} `json:"prons"`
+		Explanations []string      `json:"explanations"`
+		Synonym      []string      `json:"synonym"`
+		Antonym      []string      `json:"antonym"`
+		WqxExample   [][]string    `json:"wqx_example"`
+		Entry        string        `json:"entry"`
+		Type         string        `json:"type"`
+		Related      []interface{} `json:"related"`
+		Source       string        `json:"source"`
+	} `json:"dictionary"`
+}
+
+func query(word string) {
+	defer wg.Done()
+	client := &http.Client{}
+	request := DictRequest{Language: "en", Text: word}
+	buf, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data = bytes.NewReader(buf)
+
+	req, err := http.NewRequest("POST", "https://translate.volcengine.com/web/dict/match/v1/?msToken=&X-Bogus=DFSzKwGLQDVBKufISWQaYe9WX7Ja&_signature=_02B4Z6wo00001NVubzwAAIDAVWyVf.kf.wTVfmuAAFcz6E0NMW21knNM5R7PvNXMLZu4CNhHn3CSj5MkP2ov4L.5Nut58U-SJNR5p6FQ5YtbfuOHiq49SBrHze6EtCeVBKTttuBUhVVtq97ba3", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("authority", "translate.volcengine.com")
+	req.Header.Set("accept", "application/json, text/plain, */*")
+	req.Header.Set("accept-language", "zh-CN,zh;q=0.9")
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("cookie", "x-jupiter-uuid=16519325233891188; i18next=zh-CN; ttcid=03ce92691b254de8bf16f6c60013d36b16; s_v_web_id=verify_843e02d5c5b95c7fc3645a1bf98398ab; _tea_utm_cache_2018=undefined; tt_scid=Y6bpw3TDeClA2O45aBRCSOK4HGbi9BQGYD9ISliILB9CUVqVb3EHuDzkPGd4CT.n38b8")
+	req.Header.Set("origin", "https://translate.volcengine.com")
+	req.Header.Set("referer", "https://translate.volcengine.com/translate?category=&home_language=zh&source_language=detect&target_language=zh&text=good%0A")
+	req.Header.Set("sec-ch-ua", `" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"`)
+	req.Header.Set("sec-ch-ua-mobile", "?1")
+	req.Header.Set("sec-ch-ua-platform", `"Android"`)
+	req.Header.Set("sec-fetch-dest", "empty")
+	req.Header.Set("sec-fetch-mode", "cors")
+	req.Header.Set("sec-fetch-site", "same-origin")
+	req.Header.Set("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Mobile Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("bad StatusCode:", resp.StatusCode, "body", string(bodyText))
+	}
+	var dictResponse DictResponse
+
+	err = json.Unmarshal(bodyText, &dictResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(word, "UK:", dictResponse.Words[0].PosList[0].Phonetics[0].Text, "US:", dictResponse.Words[0].PosList[0].Phonetics[1].Text, "\n", dictResponse.Words[0].PosList[0].Explanations[0].Text)
+}
+
+func query1(word string) {
+	defer wg.Done()
+	client := &http.Client{}
+	request := DictRequest1{TransType: "en2zh", Source: word}
+	buf, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data = bytes.NewReader(buf)
+	req, err := http.NewRequest("POST", "https://api.interpreter.caiyunai.com/v1/dict", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("DNT", "1")
+	req.Header.Set("os-version", "")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
+	req.Header.Set("app-name", "xy")
+	req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("device-id", "")
+	req.Header.Set("os-type", "web")
+	req.Header.Set("X-Authorization", "token:qgemv4jr1y38jyq6vhvi")
+	req.Header.Set("Origin", "https://fanyi.caiyunapp.com")
+	req.Header.Set("Sec-Fetch-Site", "cross-site")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Referer", "https://fanyi.caiyunapp.com/")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
+	req.Header.Set("Cookie", "_ym_uid=16456948721020430059; _ym_d=1645694872")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("bad StatusCode:", resp.StatusCode, "body", string(bodyText))
+	}
+	var dictResponse DictResponse1
+	err = json.Unmarshal(bodyText, &dictResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(word, "UK:", dictResponse.Dictionary.Prons.En, "US:", dictResponse.Dictionary.Prons.EnUs)
+	for _, item := range dictResponse.Dictionary.Explanations {
+		fmt.Println(item)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, `usage: simpleDict WORD
+example: simpleDict hello
+		`)
+		os.Exit(1)
+	}
+	word := os.Args[1]
+	wg.Add(2)
+	go query(word)
+	go query1(word)
+	wg.Wait()
+}
